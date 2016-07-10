f4026f894cdb716a14915f754c894eb2ce43d2e4
tanuki2015 <75954762@qq.com>
2016-07-10 15:45:50 +0800

    ObjectId and ref

--- exp-demo/express-blog/database/index.js
+++ exp-demo/express-blog/database/index.js
@@ -16,12 +16,12 @@ const UserSchema = new mongoose.Schema({
 const UserModel = db.model('User', UserSchema);
 
 // 发表文章的schema
-const ObjectId = mongoose.Schema.Types.Objectid;
+const ObjectId = mongoose.Schema.Types.ObjectId; // 注意这个ObjectId的I是大写，坑了我几个小时啊
 const ArticleSchema = new mongoose.Schema({
   title: {type: String, require: true},
   content: {type: String, require: true},
   createAt: {type: Date, default: Date.now},
-  // author: {type: ObjectId, ref:'User'}
+  author: {type: ObjectId, ref: 'UserModel'},
 });
 const ArticleModel = db.model('Article', ArticleSchema);
 module.exports.UserModel = UserModel;
